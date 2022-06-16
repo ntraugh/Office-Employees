1344f7b52c849d4a79b4f9caacfa791902232ee7
Nate Traugh <n.traugh@yahoo.com>
2022-06-16 15:48:35 -0400

    created our list of questions for the user to input and choose from

--- index.js
+++ index.js
@@ -1,2 +1,43 @@
 const fs = require("fs")
 const inquirer = require("inquirer")
+
+
+// checking to see if the user actually input anything into the question, will run this with every question
+const checkInput = function (value) {
+    if (value != "") {
+        return true;
+    } else {
+        return "Please enter some input."
+    }
+}
+
+const questions = [
+    {
+        type: "input",
+        name: "name",
+        message: "What is the employees name?",
+        validate: checkInput
+    }, 
+    {
+        type: "input",
+        name: "Email",
+        message: "What is the employees email?",
+        validate: checkInput
+    }, 
+    {
+        type: "input",
+        name: "id",
+        message: "What is the employees ID?",
+        validate: checkInput
+    }, 
+    {
+        type: "list",
+        name: "position",
+        message: "What is the employees position?",
+        choices: [
+            "Manager", 
+            "Engineer",
+            "Intern"
+        ]
+    }, 
+]
