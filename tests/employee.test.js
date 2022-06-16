4189366ee27d13beee52694b11bb9ee79c1af52c
Nate Traugh <n.traugh@yahoo.com>
2022-06-16 16:56:53 -0400

    both tests passed as the should, need to copy pattern for ID and email as well as our functions in the employee.js file

--- tests/employee.test.js
+++ tests/employee.test.js
@@ -7,6 +7,12 @@ describe("Employee", () => {
         const employee = new Employee();
         expect(typeof(employee)).toBe("object");
     });
+    it("should set the employee's name correctly", () => { 
+        // testing to see if the employee name is the expected name
+        const name = "Nate"
+        const employee = new Employee (name);
+        expect(employee.name).toBe(name)
+    })
       
 })
 
